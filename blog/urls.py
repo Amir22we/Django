bdb964fc90df40d7cdd51e8727ed49a49c3f1a97
Amir22we <atazanovamir19@gmail.com>
2025-11-28 11:12:33 +0500

    URLs 3, done

--- blog/urls.py
+++ blog/urls.py
@@ -1,14 +1,12 @@
 from django.urls import path, include 
 from blog import views
 
-product_patterns = [
-    path('', views.products),
-    path('new/', views.new),
-    path('top/', views.top)
-]
+
 
 urlpatterns = [
     path('', views.index),
-    path('products/', include(product_patterns)),
+    path('index/<int:id>/', views.index),
+    path('access/<int:age>/', views.access),
+    
 ]
 
--- blog/views.py
+++ blog/views.py
@@ -1,13 +1,19 @@
-from django.http import HttpResponse 
+from django.http import HttpResponse, HttpResponseRedirect, HttpResponsePermanentRedirect, HttpResponseNotFound, HttpResponseBadRequest, HttpResponseForbidden
 
-def index(request):
-    return HttpResponse('Главная страница')
+def index(request, id):
+    people = [None, 'Bob', 'Sam', 'Tom']
+    # если пользователь найден возвращаем его
+    if id in range(1, len(people)):
+        return HttpResponse(people[id])
+    # если не найден то 404
+    else:
+        return HttpResponseNotFound('Not found')
 
-def products(request):
-    return HttpResponse('Список товаров')
-
-def new(request):
-    return HttpResponse('Новые товары')
-
-def top(request):
-    return HttpResponse('Популярные товары')
+def access(requestm, age):
+    # если возраст не в диапозоне 1 до 100
+    if age not in range(1, 101):
+        return HttpResponseBadRequest('Некорректные данные')
+    if age < 18:
+        return HttpResponseForbidden('Доступ заблокирован')
+    else:
+        return HttpResponse('Доступ разрешен')
