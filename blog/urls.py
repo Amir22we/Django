41b04a85afbcdf427db06603bd1585da7c4f1a8f
Amir22we <atazanovamir19@gmail.com>
2025-11-28 10:20:44 +0500

    URLs Chapter 1, done

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
-from .views import home_page_view
+from blog import views
 
 urlpatterns = [
-    path('', home_page_view, name='home'),
-    
+    path('', views.index),
+    path('about/<str:name>/<int:age>/', views.about), 
+    path('about/<str:name>/', views.about),
+    path('about/', views.about), 
+    path('contact/', views.contact),
+
 ]
--- blog/views.py
+++ blog/views.py
@@ -2,3 +2,17 @@ from django.http import HttpResponse
 
 def home_page_view(request):
     return HttpResponse('Hello, welcome to the Blog Home Page!')
+
+def index(request):
+    return HttpResponse('</h2>Welcome to the Blog Index Page</h2>')
+
+def about(request, name = 'Undefined', age = 0):
+    return HttpResponse(f'''
+                    <h2>О пользователе</h2>
+                    <p>Имя: {name}</p>
+                    <p>Возраст: {age}</p>
+                        ''')  
+
+def contact(request):
+    return HttpResponse('<h2>Contact the Blog</h2>') 
+
