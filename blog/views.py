7f801688afc7c0b1d00ea619fe84a711994f79ee
Amir22we <atazanovamir19@gmail.com>
2025-12-01 10:33:55 +0500

    templates part 1 done

--- blog/views.py
+++ blog/views.py
@@ -2,14 +2,5 @@ from django.shortcuts import render
 from django.template.response import TemplateResponse
 
 def index(request):
-    header = "Данные пользователя" # переменная
-    langs = ['Python', 'Java', 'c#'] # список
-    user = {'name': 'Tom', 'age': 22} # словарь
-    address = ('Абрикосовая', 23, 45) # кортеж
-
-    data = {'header': header, 'langs': langs, 'user': user, 'address': address}
-    return render(request, 'blog/index.html', context=data)
-
-def about(request):
-    return TemplateResponse(request, 'blog/index.html')
-
+    cat = ['Python', 'Java', 'JS', 'Go', 'C#', 'Kotlin']
+    return render(request, 'blog/index.html', context={'cat': cat})
