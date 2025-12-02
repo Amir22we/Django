5d5815a8799b79096afaff7fdf7aec553717bacb
Amir22we <atazanovamir19@gmail.com>
2025-12-02 10:42:26 +0500

    Added base.html Template

--- blog/urls.py
+++ blog/urls.py
@@ -1,11 +1,10 @@
 from django.urls import path, include, re_path
-from django.views.generic import TemplateView
 from blog import views
 
 
 
 urlpatterns = [
     path('', views.index),
-    path('about/', TemplateView.as_view(template_name='blog/about.html', extra_context={'header': 'О сайте'}))
+    path('about/', views.about),
 ]
 
--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render 
-from django.template.response import TemplateResponse
 
 def index(request):
     return render(request, 'blog/index.html')
+
+def about(request):
+    return render(request, 'blog/about.html')
