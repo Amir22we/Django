80aab2d74efcf5ddf0c4478de55ee8ff8e29a428
Amir22we <atazanovamir19@gmail.com>
2025-12-02 10:26:36 +0500

    Added TemplateView

--- blog/urls.py
+++ blog/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path, include, re_path
+from django.views.generic import TemplateView
 from blog import views
 
 
 
 urlpatterns = [
     path('', views.index),
-    path('about/', views.index)
+    path('about/', TemplateView.as_view(template_name='blog/about.html', extra_context={'header': 'О сайте'}))
 ]
 
