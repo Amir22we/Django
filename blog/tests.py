cb6337ddf212c5cdb194a74fe6497a3194c68c93
Amir22we <atazanovamir19@gmail.com>
2025-12-04 19:58:00 +0500

    Added more tests

--- blog/tests.py
+++ blog/tests.py
@@ -1,19 +1,64 @@
-from django.test import SimpleTestCase
+from django.test import SimpleTestCase, Client
+from django.views.generic import TemplateView
+
+from . import views
 
 class HomePageGetTests(SimpleTestCase):
-    def test_url_access(self):
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
         url = '/'
-        response = self.client.get(url)
-        self.assertEqual(response.status_code, 200)
+        client = Client()
+        cls.response = client.get(url)
+
+    def test_url_access(self):
+        self.assertEqual(self.response.status_code, 200)
+
+    def test_url_name(self):
+        self.assertEqual(self.response.resolver_match.url_name, 'home')
+
+    def test_url_namespace(self):
+        self.assertEqual(self.response.resolver_match.namespace, 'blog')
+
+    def test_view_name(self):
+        self.assertEqual(self.response.resolver_match.func, views.index)
 
 class AboutPageGetTests(SimpleTestCase):
-    def test_url_access(self):
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
         url = '/about/'
-        response = self.client.get(url)
-        self.assertEqual(response.status_code, 200)
-        
-class ContactPageGetTests(SimpleTestCase):
+        client = Client()
+        cls.response = client.get(url)
+            
     def test_url_access(self):
+        self.assertEqual(self.response.status_code, 200)
+
+    def test_url_name(self):
+        self.assertEqual(self.response.resolver_match.url_name, 'about')
+
+    def test_url_namespace(self):
+        self.assertEqual(self.response.resolver_match.namespace, 'blog')
+    
+    def test_view_name(self):
+        self.assertEqual(self.response.resolver_match.func.view_class, TemplateView)
+class ContactPageGetTest(SimpleTestCase):
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
         url = '/contact/'
-        response = self.client.get(url)
-        self.assertEqual(response.status_code, 302)
+        client = Client()
+        cls.response = client.get(url)
+
+    def test_url_access(self):
+        self.assertEqual(self.response.status_code, 302)
+
+    def test_url_name(self):
+        self.assertEqual(self.response.resolver_match.url_name, 'contact')
+
+    def test_url_namespace(self):
+        self.assertEqual(self.response.resolver_match.namespace, 'blog')
+
+    def test_view_name(self):
+        self.assertEqual(self.response.resolver_match.func, views.contact)
+
